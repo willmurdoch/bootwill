d6ed7cf11f019fad7e5ecd6cd5985088a5ac2d33
Will <wmurdoch@brownsteingroup.com>
2016-06-23 10:21:21 -0400

    Added phony, cleanup, mobile nav start

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
   scrollLogic();
-  filteredGrid('.bwWrap', '.filterWrap div[data-filter]', '.bw');
+  filteredGrid('.bwWrap', '.filterWrap div[data-filter]', '.tile');
 });
---
+++ phony/js/phony.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  var myHeight = $('.layer').width()*1.777;
+  $('.layer').css('height', Math.ceil(myHeight));
+  $('.phoneLayers').addClass('popOut');
+  setTimeout(function(){
+    $('.phoneLayers').addClass('noDelay');
+  })
+})
+$('.layer').click(function(){
+  if(!$(this).hasClass('active')){
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+  }
+  else{
+    $(this).removeClass('active');
+  }
+});
+$(window).resize(function(){
+  var myHeight = $('.layer').width()*1.777;
+  $('.layer').css('height', myHeight);
+})
