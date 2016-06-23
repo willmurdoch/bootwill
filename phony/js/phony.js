3caaebb743616d6eac241b84d81151e0f4b2ac0d
Will <wmurdoch@brownsteingroup.com>
2016-06-23 13:23:31 -0400

    Phony home button triggers animation

--- phony/js/phony.js
+++ phony/js/phony.js
@@ -1,10 +1,31 @@
-$(document).ready(function(){
+function triggerLayers(){
   var myHeight = $('.layer').width()*1.777;
   $('.layer').css('height', Math.ceil(myHeight));
   $('.phoneLayers').addClass('popOut');
   setTimeout(function(){
     $('.phoneLayers').addClass('noDelay');
-  },300)
+  },1000);
+}
+$('.homeBtn').click(function(){
+  if($('.phoneLayers').hasClass('popOut')){
+    if($('.layer.active').length){
+      $('.layer').removeClass('active');
+      setTimeout(function(){
+        $('.phoneLayers').removeClass('noDelay');
+        $('.phoneLayers').removeClass('popOut');
+      },300);
+    }
+    else{
+      $('.phoneLayers').removeClass('noDelay');
+      $('.phoneLayers').removeClass('popOut');
+    }
+  }
+  else{
+    triggerLayers();
+  }
+});
+$(document).ready(function(){
+  triggerLayers();
 })
 $('.layer').click(function(){
   if(!$(this).hasClass('active')){
