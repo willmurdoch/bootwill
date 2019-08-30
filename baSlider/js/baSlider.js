d8d43871039574e8cecd068f137ea0c2726a183b
Will Murdoch <wmurdoch@brownsteingroup.com>
2019-08-30 11:36:41 -0400

    Refactoring to not depend on jquery

--- baSlider/js/baSlider.js
+++
@@ -1,9 +0,0 @@
-$('.handle').mousedown(function(){
-  $('.baSlider').mousemove(function(e){
-    $('.before').css('width',e.pageX);
-  })
-})
-var unbind = function(){
-  $('.baSlider').off('mousemove');
-}
-$('.baSlider').mouseup(unbind).mouseout(unbind);
--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-$(document).ready(function(){
-  scrollLogic();
-  filteredGrid('.bwWrap', '.filterWrap div[data-filter]', '.tile');
-});
-
-$('.mobileBtn').click(function(){
-    if(!$('nav').hasClass('expanded')){
-      $('nav').addClass('expanded')
-    }
-    else $('nav').removeClass('expanded');
-
+document.getElementById('mobileBtn').addEventListener('click', function(){
+  this.classList.toggle('open');
 });
